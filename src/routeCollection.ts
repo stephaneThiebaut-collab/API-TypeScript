96b71dc08544b3dda55548bd09b711d8c3dc9530
Stephane Thiebaut <stephanethiebautjob@gmail.com>
2023-08-14 01:32:01 +0200

    Ajout de la class routeCollection pour definir les differentes routes de l'API

---
+++ src/routeCollection.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+
+type HttpVerb = "get";
+
+interface ControllerInformation  {
+    controllerName: string;
+    ctor: new (...args: any[]) => any;
+    prefix: string;
+}
+
+interface ActionInformation {
+    controllerName: string;
+    methodName: string;
+    httpVerb: HttpVerb;
+    path: string;
+}
+
+class RouteCollection {
+    private controllerInformations: ControllerInformation[];
+    private actionInformations: ActionInformation[];
+    constructor() {
+        this.controllerInformations = [];
+        this.actionInformations = [];
+    }
+    registerController(ctor: new (...args: any[]) => any, prefix: string) {
+        this.controllerInformations.push({
+            controllerName: ctor.name,
+            ctor,
+            prefix
+        });
+    }
+
+    registerAction(controllerName: string, methodName: string, httpVerb: HttpVerb, path?: string){
+        this.actionInformations.push({
+            controllerName,
+            methodName,
+            httpVerb,
+            path: path || ""
+        })
+    }
+
+    setupRouter(router: Router){
+        this.controllerInformations.forEach(c => {
+            const controller = new c.ctor();
+            const actions = this.actionInformations.filter(a => a.controllerName === c.controllerName);
+            actions.forEach(a => {
+                const action = controller[a.methodName].bind(controller);
+                const route = `/${c.prefix}/${a.path}`;
+                router[a.httpVerb](route, action);
+            });
+        })
+    }
+}
+
+const routeCollection = new RouteCollection();
+export { routeCollection, HttpVerb };
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,17 @@
 import  express  from "express";
 import { config } from "dotenv";
-import { createDbConnection } from "./middelware/mysqlConnection";
+
+import { allEmployee } from "./requete/requete";
 config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-async function main() {
-    const connection = await createDbConnection();
-
-    try {
-        const [rows, fields] = await connection.execute('SELECT * FROM employee');
-        console.log('Résultat de la requête SELECT :', rows);
-
-    } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
-    } finally {
-        await connection.end(); // Ferme la connexion à la base de données de manière asynchrone
-    }
-    }
-    main();
+app.get('/', (req, res, next) => { 
+    
+    allEmployee(res)
+})
 
 app.listen(
     port,
